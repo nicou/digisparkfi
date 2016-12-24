e0c647f4d76d7a3fc3dc006b19ebad41aa0a46fb
Nico Hagelberg <nicoustie@gmail.com>
2016-12-24 11:29:29 +0200

    Add most modifier keys

--- src/main/java/me/nicou/digisparkfi/Application.java
+++ src/main/java/me/nicou/digisparkfi/Application.java
@@ -9,6 +9,7 @@ public class Application {
 
 	private final static String SHIFT = "MOD_SHIFT_LEFT";
 	private final static String ALTGR = "MOD_ALT_RIGHT";
+	private final static String NORMALKEYS = "abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVXYZ0123456789";
 	
 	public static void main(String[] args) {
 
@@ -16,8 +17,32 @@ public class Application {
 	
 	public Map<String, SpecialCharacter> getSpecialCharacterMap() {
 		Map<String, SpecialCharacter> map = new HashMap<String, SpecialCharacter>();
-		
+		map.put("!", new SpecialCharacter("KEY_1", SHIFT));
+		map.put("\"", new SpecialCharacter("KEY_2", SHIFT));
+		map.put("#", new SpecialCharacter("KEY_3", SHIFT));
+		map.put("%", new SpecialCharacter("KEY_5", SHIFT));
+		map.put("&", new SpecialCharacter("KEY_6", SHIFT));
+		map.put("/", new SpecialCharacter("KEY_7", SHIFT));
+		map.put("(", new SpecialCharacter("KEY_8", SHIFT));
+		map.put(")", new SpecialCharacter("KEY_9", SHIFT));
+		map.put("=", new SpecialCharacter("KEY_0", SHIFT));
+		map.put("@", new SpecialCharacter("KEY_2", ALTGR));
+		map.put("$", new SpecialCharacter("KEY_4", ALTGR));
+		map.put("{", new SpecialCharacter("KEY_7", ALTGR));
+		map.put("[", new SpecialCharacter("KEY_8", ALTGR));
+		map.put("]", new SpecialCharacter("KEY_9", ALTGR));
+		map.put("}", new SpecialCharacter("KEY_0", ALTGR));
 		return map;
+	}
+	
+	public Map<String, Integer> getKeyCodes() {
+		Map<String, Integer> keycodes = new HashMap<String, Integer>();
+		keycodes.put("'", 50);
+		keycodes.put("+", 48);
+		keycodes.put(",", 54);
+		keycodes.put(".", 55);
+		keycodes.put("-", 56);
+		return keycodes;
 	}
 
 }
--- src/main/java/me/nicou/digisparkfi/SpecialCharacter.java
+++ src/main/java/me/nicou/digisparkfi/SpecialCharacter.java
@@ -2,4 +2,33 @@ package me.nicou.digisparkfi;
 
 public class SpecialCharacter {
 
+	private String key;
+	private String modifier;
+
+	public SpecialCharacter() {
+		super();
+	}
+
+	public SpecialCharacter(String key, String modifier) {
+		super();
+		this.key = key;
+		this.modifier = modifier;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getModifier() {
+		return modifier;
+	}
+
+	public void setModifier(String modifier) {
+		this.modifier = modifier;
+	}
+
 }
